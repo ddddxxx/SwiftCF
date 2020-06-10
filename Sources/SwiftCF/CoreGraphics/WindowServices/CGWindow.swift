6930679a8384bab803f19898247c52514e29420c
Xander Deng <dengxiang2010@gmail.com>
2020-06-10 17:34:04 +0800

    add documentation for ImageIO extensions

--- Sources/SwiftCF/CoreGraphics/WindowServices/CGWindow.swift
+++ Sources/SwiftCF/CoreGraphics/WindowServices/CGWindow.swift
@@ -11,6 +11,11 @@ public enum CGWindow {
 
 public extension CGWindow {
     
+    /// Return an array of window dictionaries for windows within the user
+    /// session.
+    /// This function returns NULL if the caller is not running within a Quartz
+    /// GUI session or the window server is disabled. You should release the
+    /// array when you are finished using it.
     static func windowInfoList(relativeTo window: ID = kCGNullWindowID, option: ListOption = []) -> [[InfoKey: Any]] {
         return CGWindowListCopyWindowInfo(option, window) as! [[InfoKey: Any]]? ?? []
     }
