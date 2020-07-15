661c46e98636d5ee53c3dfc1dc01bee4936fd9bf
Xander Deng <dengxiang2010@gmail.com>
2020-07-15 17:09:52 +0800

    make IOError a namespace

--- Sources/SwiftCF/IOKit/IOError.swift
+++ Sources/SwiftCF/IOKit/IOError.swift
@@ -5,119 +5,125 @@ import IOKit
 extension IOReturn {
     
     @usableFromInline func throwIfIOError() throws {
-        if let err = IOError(rawValue: self) {
+        if let err = KernelError(rawValue: self) {
             throw err
         }
     }
 }
 
-public typealias IOError = KernelError
+/// A namespace for IOReturn errors.
+public enum IOError {}
 
 public extension IOError {
     /// General error
-    static let ioError = IOError(rawValue: kIOReturnError)!
+    static let error = KernelError(rawValue: kIOReturnError)!
     /// Can't allocate memory
-    static let ioNoMemory = IOError(rawValue: kIOReturnNoMemory)!
+    static let noMemory = KernelError(rawValue: kIOReturnNoMemory)!
     /// Resource shortage
-    static let ioNoResources = IOError(rawValue: kIOReturnNoResources)!
+    static let noResources = KernelError(rawValue: kIOReturnNoResources)!
     /// Error during IPC
-    static let ioIPCError = IOError(rawValue: kIOReturnIPCError)!
+    static let ipcError = KernelError(rawValue: kIOReturnIPCError)!
     /// No such device
-    static let ioNoDevice = IOError(rawValue: kIOReturnNoDevice)!
+    static let noDevice = KernelError(rawValue: kIOReturnNoDevice)!
     /// Privilege violation
-    static let ioNotPrivileged = IOError(rawValue: kIOReturnNotPrivileged)!
+    static let notPrivileged = KernelError(rawValue: kIOReturnNotPrivileged)!
     /// Invalid argument
-    static let ioBadArgument = IOError(rawValue: kIOReturnBadArgument)!
+    static let badArgument = KernelError(rawValue: kIOReturnBadArgument)!
     /// Device read locked
-    static let ioLockedRead = IOError(rawValue: kIOReturnLockedRead)!
+    static let lockedRead = KernelError(rawValue: kIOReturnLockedRead)!
     /// Device write locked
-    static let ioLockedWrite = IOError(rawValue: kIOReturnLockedWrite)!
+    static let lockedWrite = KernelError(rawValue: kIOReturnLockedWrite)!
     /// Exclusive access and device already open
-    static let ioExclusiveAccess = IOError(rawValue: kIOReturnExclusiveAccess)!
+    static let exclusiveAccess = KernelError(rawValue: kIOReturnExclusiveAccess)!
     /// Sent/received messages had different msg_id
-    static let ioBadMessageID = IOError(rawValue: kIOReturnBadMessageID)!
+    static let badMessageID = KernelError(rawValue: kIOReturnBadMessageID)!
     /// Unsupported function
-    static let ioUnsupported = IOError(rawValue: kIOReturnUnsupported)!
+    static let unsupported = KernelError(rawValue: kIOReturnUnsupported)!
     /// Misc. VM failure
-    static let ioVMError = IOError(rawValue: kIOReturnVMError)!
+    static let vmError = KernelError(rawValue: kIOReturnVMError)!
     /// Internal error
-    static let ioInternalError = IOError(rawValue: kIOReturnInternalError)!
+    static let internalError = KernelError(rawValue: kIOReturnInternalError)!
     /// General I/O error
-    static let ioIOError = IOError(rawValue: kIOReturnIOError)!
+    static let ioError = KernelError(rawValue: kIOReturnIOError)!
     /// Can't acquire lock
-    static let ioCannotLock = IOError(rawValue: kIOReturnCannotLock)!
+    static let cannotLock = KernelError(rawValue: kIOReturnCannotLock)!
     /// Device not open
-    static let ioNotOpen = IOError(rawValue: kIOReturnNotOpen)!
+    static let notOpen = KernelError(rawValue: kIOReturnNotOpen)!
     /// Read not supported
-    static let ioNotReadable = IOError(rawValue: kIOReturnNotReadable)!
+    static let notReadable = KernelError(rawValue: kIOReturnNotReadable)!
     /// Write not supported
-    static let ioNotWritable = IOError(rawValue: kIOReturnNotWritable)!
+    static let notWritable = KernelError(rawValue: kIOReturnNotWritable)!
     /// Alignment error
-    static let ioNotAligned = IOError(rawValue: kIOReturnNotAligned)!
+    static let notAligned = KernelError(rawValue: kIOReturnNotAligned)!
     /// Media error
-    static let ioBadMedia = IOError(rawValue: kIOReturnBadMedia)!
+    static let badMedia = KernelError(rawValue: kIOReturnBadMedia)!
     /// Device(s) still open
-    static let ioStillOpen = IOError(rawValue: kIOReturnStillOpen)!
+    static let stillOpen = KernelError(rawValue: kIOReturnStillOpen)!
     /// RLD failure
-    static let ioRLDError = IOError(rawValue: kIOReturnRLDError)!
+    static let rldError = KernelError(rawValue: kIOReturnRLDError)!
     /// DMA failure
-    static let ioDmaError = IOError(rawValue: kIOReturnDMAError)!
+    static let dmaError = KernelError(rawValue: kIOReturnDMAError)!
     /// Device busy
-    static let ioBusy = IOError(rawValue: kIOReturnBusy)!
+    static let busy = KernelError(rawValue: kIOReturnBusy)!
     /// I/O timeout
-    static let ioTimeout = IOError(rawValue: kIOReturnTimeout)!
+    static let timeout = KernelError(rawValue: kIOReturnTimeout)!
     /// Device offline
-    static let ioOffline = IOError(rawValue: kIOReturnOffline)!
+    static let offline = KernelError(rawValue: kIOReturnOffline)!
     /// Not ready
-    static let ioNotReady = IOError(rawValue: kIOReturnNotReady)!
+    static let notReady = KernelError(rawValue: kIOReturnNotReady)!
     /// Device not attached
-    static let ioNotAttached = IOError(rawValue: kIOReturnNotAttached)!
+    static let notAttached = KernelError(rawValue: kIOReturnNotAttached)!
     /// No DMA channels left
-    static let ioNoChannels = IOError(rawValue: kIOReturnNoChannels)!
+    static let noChannels = KernelError(rawValue: kIOReturnNoChannels)!
     /// No space for data
-    static let ioNoSpace = IOError(rawValue: kIOReturnNoSpace)!
+    static let noSpace = KernelError(rawValue: kIOReturnNoSpace)!
     /// Port already exists
-    static let ioPortExists = IOError(rawValue: kIOReturnPortExists)!
+    static let portExists = KernelError(rawValue: kIOReturnPortExists)!
     /// Can't wire down physical memory
-    static let ioCannotWire = IOError(rawValue: kIOReturnCannotWire)!
+    static let cannotWire = KernelError(rawValue: kIOReturnCannotWire)!
     /// No interrupt attached
-    static let ioNoInterrupt = IOError(rawValue: kIOReturnNoInterrupt)!
+    static let noInterrupt = KernelError(rawValue: kIOReturnNoInterrupt)!
     /// No DMA frames enqueued
-    static let ioNoFrames = IOError(rawValue: kIOReturnNoFrames)!
+    static let noFrames = KernelError(rawValue: kIOReturnNoFrames)!
     /// Oversized msg received on interrupt port
-    static let ioMessageTooLarge = IOError(rawValue: kIOReturnMessageTooLarge)!
+    static let messageTooLarge = KernelError(rawValue: kIOReturnMessageTooLarge)!
     /// Not permitted
-    static let ioNotPermitted = IOError(rawValue: kIOReturnNotPermitted)!
+    static let notPermitted = KernelError(rawValue: kIOReturnNotPermitted)!
     /// No power to device
-    static let ioNoPower = IOError(rawValue: kIOReturnNoPower)!
+    static let noPower = KernelError(rawValue: kIOReturnNoPower)!
     /// Media not present
-    static let ioNoMedia = IOError(rawValue: kIOReturnNoMedia)!
+    static let noMedia = KernelError(rawValue: kIOReturnNoMedia)!
     /// media not formatted
-    static let ioUnformattedMedia = IOError(rawValue: kIOReturnUnformattedMedia)!
+    static let unformattedMedia = KernelError(rawValue: kIOReturnUnformattedMedia)!
     /// No such mode
-    static let ioUnsupportedMode = IOError(rawValue: kIOReturnUnsupportedMode)!
+    static let unsupportedMode = KernelError(rawValue: kIOReturnUnsupportedMode)!
     /// Data underrun
-    static let ioUnderrun = IOError(rawValue: kIOReturnUnderrun)!
+    static let underrun = KernelError(rawValue: kIOReturnUnderrun)!
     /// Data overrun
-    static let ioOverrun = IOError(rawValue: kIOReturnOverrun)!
+    static let overrun = KernelError(rawValue: kIOReturnOverrun)!
     /// The device is not working properly
-    static let ioDeviceError = IOError(rawValue: kIOReturnDeviceError)!
+    static let deviceError = KernelError(rawValue: kIOReturnDeviceError)!
     /// A completion routine is required
-    static let ioNoCompletion = IOError(rawValue: kIOReturnNoCompletion)!
+    static let noCompletion = KernelError(rawValue: kIOReturnNoCompletion)!
     /// Operation aborted
-    static let ioAborted = IOError(rawValue: kIOReturnAborted)!
+    static let aborted = KernelError(rawValue: kIOReturnAborted)!
     /// Bus bandwidth would be exceeded
-    static let ioNoBandwidth = IOError(rawValue: kIOReturnNoBandwidth)!
+    static let noBandwidth = KernelError(rawValue: kIOReturnNoBandwidth)!
     /// Device not responding
-    static let ioNotResponding = IOError(rawValue: kIOReturnNotResponding)!
+    static let notResponding = KernelError(rawValue: kIOReturnNotResponding)!
     /// Isochronous I/O request for distant past
-    static let ioIsoTooOld = IOError(rawValue: kIOReturnIsoTooOld)!
+    static let isoTooOld = KernelError(rawValue: kIOReturnIsoTooOld)!
     /// Isochronous I/O request for distant future
-    static let ioIsoTooNew = IOError(rawValue: kIOReturnIsoTooNew)!
+    static let isoTooNew = KernelError(rawValue: kIOReturnIsoTooNew)!
     /// Data was not found
-    static let ioNotFound = IOError(rawValue: kIOReturnNotFound)!
+    static let notFound = KernelError(rawValue: kIOReturnNotFound)!
     /// Should never be seen
-    static let ioInvalid = IOError(rawValue: kIOReturnInvalid)!
+    static let invalid = KernelError(rawValue: kIOReturnInvalid)!
+}
+
+public extension KernelError {
+    /// A namespace for IOReturn errors.
+    static let io = IOError.self
 }
 
 #endif // canImport(IOKit)
