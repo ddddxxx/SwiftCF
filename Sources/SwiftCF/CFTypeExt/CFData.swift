f8be6fc640b9ed5c3738ae169833861f35c7d9c1
Xander Deng <dengxiang2010@gmail.com>
2020-10-21 14:55:40 +0800

    group files

--- Sources/SwiftCF/CFTypeExt/CFData.swift
+++ Sources/SwiftCF/CFTypeExt/CFData.swift
@@ -48,3 +48,37 @@ extension CFData: RandomAccessCollection {
         return bytePtr[position]
     }
 }
+
+// MARK: - CFMutableData
+
+public extension CFMutableData {
+    
+    @inlinable static func create(allocator: CFAllocator = .default, capacity: CFIndex = 0) -> CFMutableData {
+        return CFDataCreateMutable(allocator, capacity)
+    }
+    
+    @inlinable var mutableBytePtr: UnsafeMutableBufferPointer<UInt8> {
+        let p = CFDataGetMutableBytePtr(self)
+        return UnsafeMutableBufferPointer(start: p, count: length)
+    }
+    
+    @inlinable func setLength(_ length: CFIndex) {
+        CFDataSetLength(self, length)
+    }
+    
+    @inlinable func increaseLength(_ extraLength: CFIndex) {
+        CFDataIncreaseLength(self, extraLength)
+    }
+    
+    @inlinable func append(bytes: UnsafePointer<UInt8>, length: CFIndex) {
+        CFDataAppendBytes(self, bytes, length)
+    }
+    
+    @inlinable func replaceBytes(range: CFRange, newBytes: UnsafePointer<UInt8>, length: CFIndex) {
+        CFDataReplaceBytes(self, range, newBytes, length)
+    }
+    
+    @inlinable func deleteBytes(range: CFRange) {
+        CFDataDeleteBytes(self, range)
+    }
+}
